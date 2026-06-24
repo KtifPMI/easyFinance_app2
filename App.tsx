bd71d7fce756c8e067e2a55b4251af0715b2c91b
KtifPMI <leha.grigorev.02@mail.ru>
2026-06-24 14:37:53 +0300

    fix: add expo-font dep, fix babel-preset-expo version, cleanup App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,3 @@
-import 'react-native-gesture-handler';
-import 'react-native-screens';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
