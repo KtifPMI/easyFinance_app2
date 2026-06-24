f2b39534bb3706b7e19ce78e746307d618cf362b
Your Name <you@example.com>
2026-06-24 12:07:58 +0300

    fix: add required native imports at entry point

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+import 'react-native-screens';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
